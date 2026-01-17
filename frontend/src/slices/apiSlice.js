e2e3e796357577d193358ac4cedcdc48b09842fa
Atta Muhammad <attamuhammad7587@gmail.com>
2026-01-17 18:07:48 +0500

    Update apiSlice to use environment variable

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+// 👇 Change this line to use the Environment Variable
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: process.env.REACT_APP_API_URL || "" 
+});
 
 export const apiSlice = createApi({
   baseQuery,
-  // We include 'DailyLog' here to ensure caching works for all features
   tagTypes: ["User", "Workout", "DailyLog"], 
   endpoints: (builder) => ({}),
 });
